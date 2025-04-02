12ee4671efd7ac63a1a1c3c5f35d3ce47c599e41
shreya-mehra-97 <shreya10.mehra@gmail.com>
2025-04-02 21:24:10 +0530

    versio updates

--- src/components/APICall.js
+++ src/components/APICall.js
@@ -1,6 +1,6 @@
 export default async function APICall(_path, _method, _body) {
-    // const ROOT_URL = 'https://rentflix-backend.vercel.app'
-    const ROOT_URL = 'http://localhost:5000'
+    const ROOT_URL = 'https://renflix-backend.onrender.com'
+    // const ROOT_URL = 'http://localhost:5000'
     const response = await fetch(ROOT_URL + _path, {
         method: _method,
         headers: {
